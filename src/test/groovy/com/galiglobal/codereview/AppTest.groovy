fb0a4be41e756cab54e228a7339c78682916b965
antonmry <anton@galiglobal.com>
2020-11-20 21:21:15 +0100

    Initial version

---
+++ src/test/groovy/com/galiglobal/codereview/AppTest.groovy
@@ -0,0 +1,16 @@
+package com.galiglobal.codereview
+
+import spock.lang.Specification
+
+class AppTest extends Specification {
+    def "application has a greeting"() {
+        setup:
+        def app = new App()
+
+        when:
+        def result = app.greeting
+
+        then:
+        result != null
+    }
+}
